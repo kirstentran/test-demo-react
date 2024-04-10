da93f4d5b66d9086dd199a60496ef5cbe914e41c
Kirsten Tran <uyen.tran@usm.edu>
2024-04-10 01:41:57 -0500

    video 18

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,12 +10,23 @@ class MyComponent extends React.Component {
         address: 'Uyen',
         age: 24
     };
+
+    handleClick(event) {
+        console.log(">> click me my button");
+        // console.log(event)
+    }
+
+    handleOneMoverOver(event) {
+        console.log(event.pageX)
+    }
     //JSX
     render() {
         return (
 
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onMouseOver={this.handleOneMoverOver}>Hover me</button>
+                <button onClick={this.handleClick}>Click me</button>
             </div>
 
         );
